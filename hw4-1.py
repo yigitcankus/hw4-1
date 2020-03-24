8ab8d81b782dbe108d7559a49016a4a8e84a15f1
Yiğit Can Kuş <57319487+yigitcankus@users.noreply.github.com>
2020-03-24 17:44:44 +0300

    Add files via upload

---
+++ hw4-1.py
@@ -0,0 +1,29 @@
+##Keşifsel veri analizinin amacı ham veriden kullanışlı bir veri oluşturmaktır.
+
+
+######
+"""
+Bir e-ticaret şirketinin ürünlerine ait müşteri yorumlarını içeren bir
+veri kümesi üzerinde çalıştığınızı farz edin.
+ Şirketin web sitesi aracılığıyla toplanan müşteri yorumları içeriğinin olumlu ya da
+ olumsuz olmasına göre 1 ila 5 arasında derecelendirilmiştir.
+
+a. Bu ham veride ne tür problemlerle karşılaşmayı beklersiniz?
+"""
+## a) Sadece kötülemek için kötü puanlanmış olabilir.
+##    Şirket kötü yorumları sildiği için sadece iyi yorumlar bulunuyor olabilir
+##    Bot yorumları olabilir.
+
+"""
+b. Göreviniz müşterilerin yorumlarının olumlu yada olumsuz olduğunu ortaya çıkaran özellikleri belirlemek olsaydı 
+nasıl bir yaplaşımda bulunur ve bunun için hangi metodları kullanırdınız?
+"""
+## Bazı kelimeleri cümle içinde arayabiliriz i.e. iyi, güzel, temiz, kötü, hatalı
+
+"""
+Ham veriden çıkarılabilecek yararlı özelliklerin neler olabilir? 
+Bu verilere nasıl ulaşılabilirsiniz ve bunların faydalı olup olmayacağını nasıl anlayabilirsiniz?
+"""
+
+## eğer sayılı bir cümle varsa neyin sayısını verdiğini görebilirz
+## cümleleri okumamız lazım.
